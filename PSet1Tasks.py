15f5f1f88af427a66bc7e5a584f71602f4eccb25
Nikolay <noilgt@gmail.com>
2015-01-20 00:18:13 +0300

    Initial commit
    Init of repo

---
+++ PSet1Tasks.py
@@ -0,0 +1,58 @@
+s = 'azcbobobegghakl'
+target = 'bob'
+lenSource = len(s)
+lenTarget = len(target)
+curPos = 0
+cntFounded = 0
+iteration = 0
+
+# Task 2 BEGIN
+# Main idea:
+# 1.Take slice from curPos symbol to lenTarget
+# 2. Compare with target
+# 	if <> then:
+#		incremet counter
+#		move curPos to next symbol 
+# 		and repeat steps 1 .. 2
+#
+
+#while curPos < lenSource:
+#	if s[curPos:lenTarget+iteration] == target:
+#		cntFounded += 1
+#	curPos += 1
+#	iteration += 1
+
+#print "Number of times " + str(target) + "occurs is: " + str(cntFounded)
+# Task 2 END
+
+
+#Task 3
+strAlphaOrder = ''
+# find symbol position in alphabet
+def findPosInAlpha (curSymbol):
+	alphabet = 'abcdefghijklmnopqrstuywxyz'
+	curPos = 0
+	for symbol in alphabet:
+		if symbol == curSymbol:
+			return curPos
+		curPos += 1
+#get next symbol
+#def getNextSymbol (index, strSource):
+#	return strSource[index+1]
+
+index = 0
+nextIndex = 0
+curPos = 0
+lenStrAlpha = 0
+iteration = 0
+# TEST: print findPosInAlpha('d')
+while curPos < lenSource:
+	
+	index = findPosInAlpha(s[curPos+iteration])
+	nextIndex = findPosInAlpha(s[curPos+iteration+1])
+	if nextIndex >= index:
+		strAlphaOrder = s[curPos: curPos+2]
+	curPos +=1
+	iteration +=1
+
+print strAlphaOrder
