d1b58e651f74fab6cd128d21806089093c9ffd26
Nikolay <noilgt@gmail.com>
2015-01-21 00:19:34 +0300

    Refactor

--- PSet2Task1.py
+++ PSet2Task1.py
@@ -2,13 +2,17 @@ balance = 4213
 annualInterestRate = 0.2
 monthlyPaymentRate = 0.04
 
+monthlyInterestRate = annualInterestRate /12.0
 month = 1
-totalPaid = 0
+totalPaid = 0.0
 while month <= 12:
     
     previousBalance = balance    
-    monthlyInterestRate = annualInterestRate /12.0
+    
     minimumMonthlyPaymentRate = monthlyPaymentRate * previousBalance
+    
+    totalPaid += minimumMonthlyPaymentRate
+    
     monthlyUnpaidBalance = previousBalance - minimumMonthlyPaymentRate
     updatedBalance = monthlyUnpaidBalance + monthlyInterestRate * monthlyUnpaidBalance
     balance = updatedBalance
@@ -17,7 +21,7 @@ while month <= 12:
     print 'Minimum monthly payment: ' + str(round(minimumMonthlyPaymentRate, 2))
     print 'Remaining balance: ' + str(round(updatedBalance, 2) )
     if month == 12:
-        print 'Total paid: ' + str(round(totalPaid))
+        print 'Total paid: ' + str(round(totalPaid, 2))
         print 'Remaining balance: ' + str(round(updatedBalance, 2) )
         break
         
