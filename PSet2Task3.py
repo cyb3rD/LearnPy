977fb98d5703be15618e77cb48af369ef6873b3b
Nikolay <noilgt@gmail.com>
2015-01-22 00:19:00 +0300

    remove comments

--- PSet2Task3.py
+++ PSet2Task3.py
@@ -2,8 +2,8 @@
 # Increse speed of computation fixed monthly payment by using bisectional search
 #
 
-balance = 3926 # comment beforesubmission
-annualInterestRate = 0.2 # comment beforesubmission
+balance = 999999 # comment beforesubmission
+annualInterestRate = 0.18 # comment beforesubmission
 
 monthlyInterestRate = annualInterestRate /12.0
 
@@ -11,10 +11,20 @@ month = 1
 totalPaid = 0.0
 
 initialBalance = balance #save initial balance
-fixedMonthlyPayment = 10 #startig point
+# init data for bisectional search
+monthlyPaymentLowerBound = balance / 12.0
+monthlyPaymentUpperBound =  (balance * (1 + monthlyInterestRate)**12) / 12.0
+
+tolerance = 0.01
+
+def getPayment(min, max):
+    return (min+max)/2.0
+
+initFixedMonthlyPayment =  getPayment(monthlyPaymentLowerBound, monthlyPaymentUpperBound)  
+fixedMonthlyPayment = initFixedMonthlyPayment
+
 previousBalance = initialBalance
- 
-#check balance with fixed payment 
+
 while month <= 12:
 
     totalPaid += fixedMonthlyPayment
@@ -24,8 +34,32 @@ while month <= 12:
     updatedBalance = monthlyUnpaidBalance + monthlyInterestRate * monthlyUnpaidBalance
     previousBalance = updatedBalance
         
-    if month == 12 and updatedBalance > 0:
-        fixedMonthlyPayment += 10
+    if month == 12:
+        #
+        # Find new fixed payment (fixedMonthlyPayment)
+        #
+        print 'Total paid: ' + str(totalPaid)
+        
+        #if totalPaid > initialBalance:
+            
+        
+        
+        diffPaid = totalPaid - initialBalance
+        print 'diff: ' + str(diffPaid)
+        if (diffPaid <= tolerance):
+            print 'Payment: ' + str(fixedMonthlyPayment) + ' diff: ' + str(diffPaid) + ' Payment: '+ str(fixedMonthlyPayment)
+            break
+        if (diffPaid < 0):
+            # min = fixedPayment
+            print 'Payment: ' + str(fixedMonthlyPayment) + ' diff: ' + str(diffPaid) + ' Payment: '+ str(fixedMonthlyPayment)
+            fixedMonthlyPayment = getPayment(fixedMonthlyPayment, monthlyPaymentUpperBound)
+        #
+        if (diffPaid > 0):
+            # max = fixedPayment
+            print 'Payment: ' + str(fixedMonthlyPayment) + ' diff: ' + str(diffPaid) + ' Payment: '+ str(fixedMonthlyPayment)
+            fixedMonthlyPayment = getPayment(monthlyPaymentLowerBound, fixedMonthlyPayment)   
+        
+        # Next check with new fixed payment
         totalPaid = 0
         month = 1
         previousBalance = initialBalance
