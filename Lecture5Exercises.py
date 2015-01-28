82ab155b43767df30ca57f451c03a485ff7f1a8e
Nikolay Ignatiev <noilgt@gmail.com>
2015-01-28 14:23:37 +0300

    Initial of the Lecture5 exercises

---
+++ Lecture5Exercises.py
@@ -0,0 +1,18 @@
+def iterMul(a, b):
+    result = 0
+    while b > 0:
+        result += a
+        b -= 1
+    return result
+    
+    
+def iterPower(base, exp):
+    '''
+    exp: int >= 0
+    returns: int or float, base^exp    base: int or float.
+    '''
+    result = 1
+    while exp > 0:
+        result *= base
+        exp = exp -1
+    return result  
