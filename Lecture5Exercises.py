ba28861f92ebbf603fea212ef08aeee93d5fece7
Nikolay <noilgt@gmail.com>
2015-01-29 23:20:50 +0300

    Added iterPower()
    My version and Grade

--- Lecture5Exercises.py
+++ Lecture5Exercises.py
@@ -4,7 +4,7 @@ def iterMul(a, b):
         result += a
         b -= 1
     return result
-    
+  
     
 def iterPower(base, exp):
     '''
@@ -15,4 +15,32 @@ def iterPower(base, exp):
     while exp > 0:
         result *= base
         exp = exp -1
-    return result  
+    return result 
+    
+def gcdIter(a, b):
+    '''
+    a, b: positive integers
+    
+    returns: a positive integer, the greatest common divisor of a & b.
+    '''
+    testValue  = min(a,b)
+    if testValue == 1 :
+        return b     
+    
+    while testValue > 1:
+        if ((a % testValue) or (b % testValue)) == 0:
+            return testValue
+        else:
+            testValue -=1
+    
+    return testValue
+    
+    # Grade Aswer 
+    #testValue = min(a, b)
+    #
+    # Keep looping until testValue divides both a & b evenly
+    #while a % testValue != 0 or b % testValue != 0:
+    #    testValue -= 1
+    #
+    #return testValue
+    
