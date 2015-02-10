61eb31c3e7839bb4edd9f119e13465151b9c1bdb
Nikolay Ignatiev <noilgt@gmail.com>
2015-02-10 11:11:30 +0300

    Lecture Exercises

--- Lecture5Exercises.py
+++ Lecture5Exercises.py
@@ -35,7 +35,7 @@ def gcdIter(a, b):
     
     return testValue
     
-    # Grade Aswer 
+    # Grade Answer 
     #testValue = min(a, b)
     #
     # Keep looping until testValue divides both a & b evenly
@@ -55,4 +55,77 @@ def gcdRecur(a, b):
         return a
     else:
         return gcdRecur(b, a % b)   
+
+#L5 Problem 8    
+def isIn(char, aStr):
+    '''
+    char: a single character
+    aStr: an alphabetized string
     
+    returns: True if char is in aStr; False otherwise
+    '''
+    # BASE Case: Symbol founded
+    if char > aStr[len(aStr)-1]:
+        return False
+    # heck 1st, middle, last
+    if ( (char == aStr[0]) or (char == aStr[len(aStr)-1]) or (char == aStr[(len(aStr)/2)-1]) ):
+        return True
+    else:
+        if char < aStr[len(aStr)/2]:
+            # compare with middle symbol of the 1st part
+            isIn(char,aStr[0:len(aStr)/2])
+        else:
+            # compare with middle symbol of the second part
+            isIn(char,aStr[len(aStr)/2:])
+    # NOT founded            
+    return False
+print "Test cases for isIn(char, aStr) recurcsive:"
+print 'Case: isIn(\'a\',\'abcdef\')' + 'Result: ' + str(isIn('a','abcdef'))
+print 'Case: isIn(\'c\',\'abcdef\')' + 'Result: ' + str(isIn('c','abcdef'))
+print 'Case: isIn(\'f\',\'abcdef\')' + 'Result: ' + str(isIn('f','abcdef'))
+print 'Case: isIn(\'h\',\'abcdef\')' + 'Result: ' + str(isIn('h','abcdef'))
+
+#L5 Problem 9
+def semordnilapWrapper(str1, str2):
+    # A single-length string cannot be semordnilap
+    if len(str1) == 1 or len(str2) == 1:
+        return False
+
+    # Equal strings cannot be semordnilap
+    if str1 == str2:
+        return False
+
+    return semordnilap(str1, str2)
+
+def semordnilap(str1, str2):
+    '''
+    str1: a string
+    str2: a string
+    
+    returns: True if str1 and str2 are semordnilap;
+             False otherwise.
+    '''
+    str1Pos = 0
+    str2Pos = len(str2)-1
+    # check for single-length, equal strings, length of strings
+    if (len(str1) != len(str2)):
+        return False
+    # check if two strings are palindromes
+    else:
+        if str1[str1Pos] != str2[str2Pos]:
+            #return False
+            isPalindrome = False
+            #break
+        else:
+        #if str1[0:str1Pos] == str2[str2Pos:]:
+            str1Pos += 1
+            str2Pos -= 1
+            semordnilapWrapper(str1[str1Pos:], str2[0:str2Pos+1])
+            
+            
+print  semordnilap('live', 'evil')  
+print  semordnilap('lives', 'evil') 
+print  semordnilap('liv', 'evil')
+print  semordnilap('love', 'evil')
+            
+            
---
+++ Lecture6Exercises.py
@@ -0,0 +1,11 @@
+'''
+Here we get execrsises fo Lection 6
+'''
+def oddTuples(aTup):
+    '''
+    aTup: a tuple
+    
+    returns: tuple, every other element of aTup. 
+    '''
+    return aTup[0::2]
+
