4833ad5a876500e27a2beb8969982637d4dde560
Nikolay Ignatiev <noilgt@gmail.com>
2015-02-18 12:28:14 +0300

    Some bug searching and correcting it

--- lectureCode_Lec7Tests.py
+++ lectureCode_Lec7Tests.py
@@ -1,18 +1,33 @@
 def isPal(x):
     assert type(x) == list
-    temp = x
-    temp.reverse
+    #temp = x
+    
+    temp = x[:] # Make a copy of source list
+    
+    # 4. Try to find wjere the bug is
+    #print(temp, x)
+    #temp.reverse # BUG 2 with reversion of temp
+    
+    temp.reverse() # Correct string
+    
+    # 3. Try to find wjere the bug is
+    #print(temp, x)
     if temp == x:
         return True
     else:
         return False
 
 def silly(n):
+    result = [] # FIXED initialisation
     for i in range(n):
-        result = []
+        #result = [] # BUG 1 - always init with empty lis
         elem = raw_input('Enter element: ')
         result.append(elem)
+        # 2. Try to find wjere the bug is
+        print(result)
     if isPal(result):
+        # 1. Try to find wjere the bug is
+        print(result)
         print('Yes')
     else:
         print('No')
