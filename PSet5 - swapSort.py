3f8beab6cd73db4b9d4fcf9ec4afee8310807ef1
Nikolay Ignatiev <noilgt@gmail.com>
2015-02-18 12:31:53 +0300

    Problem Set 5 swapSort algorithm

---
+++ PSet5 - swapSort.py
@@ -0,0 +1,34 @@
+def swapSort(L): 
+    """ L is a list on integers """
+    print "Original L: ", L
+    for i in range(len(L)):
+        for j in range(len(L)):
+            if L[j] < L[i]:
+                # the next line is a short 
+                # form for swap L[i] and L[j]
+                L[j], L[i] = L[i], L[j] 
+                print L
+    print "Final L: ", L
+    
+#swapSort([])
+
+def search(L, e):
+    for i in range(len(L)):
+        if L[i] == e:
+            return True
+        if L[i] > e:
+            return False
+    return False
+    
+print search([1,3],3)
+
+def newSearch(L, e):
+    size = len(L)
+    for i in range(size):
+        if L[size-i-1] == e:
+            return True
+        if L[i] < e:
+            return False
+    return False
+    
+print newSearch([1,3],3)
