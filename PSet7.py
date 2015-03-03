965e01d8840d5d32b65a2d831fbdd806bc362d29
Nikolay Ignatiev <noilgt@gmail.com>
2015-03-03 19:22:43 +0300

    Triggers realisation.
    v1 of triggers realisation

--- PSet7.py
+++ PSet7.py
@@ -1,6 +1,12 @@
-'''
-Problem Set 6: RSS Feed Classes
-'''
+#======================
+# Part 1
+# Data structure design
+#======================
+
+# Problem 1
+
+# NewsStory
+
 class NewsStory(object):
 	def __init__(self, guid, title, subject, summary, link):
 		self.newsGuid = guid
@@ -19,5 +25,54 @@ class NewsStory(object):
 		return self.newsLink
 	def getSummary(self):
 		return self.newsSummary
+#======================
+# Part 2
+# Triggers
+#======================
+
+class Trigger(object):
+    def evaluate(self, story):
+        """
+        Returns True if an alert should be generated
+        for the given news item, or False otherwise.
+        """
+        raise NotImplementedError
+
+# Whole Word Triggers
+# Problems 2-5
+
+# WordTrigger
+class WordTrigger(Trigger):
+	def __init__(self, word):
+		self.word = word
+
+	def isWordIn(text):
+		myText = ''
+		result = false
+		for w in text.split():
+			for s in string.punctuation:
+				if s in w:
+					myText += w.replace(s, ' ')
+		if self.word in myText:
+			result = True
+		return result
+
+# TitleTrigger
+class TitleTrigger(WordTrigger):
+	def evaluate(self,word,title):
+		WordTrigger(word)
+		return WordTrigger.isWordIn(title)
+# SubjectTrigger
+class SubjectTrigger(WordTrigger):
+	def evaluate(self, word, subject):
+		WordTrigger(word)
+		return WordTrigger.isWordIn(subject)
+
+# SummaryTrigger
+class SummaryTrigger(WordTrigger):
+	def evaluate(self, word, summary):
+		WordTrigger(word)
+		return WordTrigger.isWordIn(summary)
+
 
 
